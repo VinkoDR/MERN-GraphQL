a23afefd1453ae30a28ddcabd72ad83b50e48735
Vinko Roditi <vinko.d.roditi@gmail.com>
2022-07-31 19:49:24 +0200

    feature: project and clients relationship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const { projects, clients } = require("../sampleData.js")
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require("graphql")
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require("graphql")
 
 // Client Type
 const ClientType = new GraphQLObjectType({
@@ -14,9 +14,53 @@ const ClientType = new GraphQLObjectType({
     
 })
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+    name : "Project",
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString},
+        status: {type: GraphQLString},
+        //relation to client
+        client: {
+            type: ClientType,
+            resolve(parent, args){
+                //clientId est une clé secondaire dans les datas de projects
+                return clients.find(client => client.id === parent.clientId)
+            }}
+    })
+
+    
+})
+
 const RootQuery = new GraphQLObjectType({
     name : "RootQuerytype",
     fields: {
+         //query get all projects
+         projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parents,args){
+               return projects
+            }
+        },
+        //query get single project
+        project: {
+            type: ProjectType,
+            args: {id :{type: GraphQLID}},
+            resolve(parents,args){
+                //later we will use mongoose to get a single client...
+                return projects.find(project => project.id === args.id)
+            }
+        },
+        //query get all clients
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parents,args){
+               return clients
+            }
+        },
+        //query get single client
         client: {
             type: ClientType,
             args: {id :{type: GraphQLID}},
